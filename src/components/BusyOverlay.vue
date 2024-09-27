5987d130da552d482c10137eede1ef7161a46740
Sebastian Eichelbaum <code@nemtics.com>
2024-09-27 11:19:17 +0200

    Add: seperate busy indicator

--- src/components/BusyOverlay.vue
+++ src/components/BusyOverlay.vue
@@ -2,7 +2,14 @@
 A nice busy overlay that wraps the whole slot content and shades it a bit.
 
 * The anonymous slot is the content itself.
-* Slot "overlay" is the arbitrary content to show on the overlay if busy. Is a spinner by default.
+* Slot "overlay" is the arbitrary content to show on the overlay if busy. Is a spinner by default. Passes "message" and "percent"
+
+Example:
+
+<BusyOverlay :busy="true" msg="common.msg.greet" :percent="12">
+    <template #overlay="{ message, percent }"> Textspinner huiiii: {{ percent }} {{ message }} </template>
+</BusyOverlay>
+
 -->
 <template>
     <div id="container">
@@ -10,25 +17,11 @@ A nice busy overlay that wraps the whole slot content and shades it a bit.
             <slot>TODO: Put something in here.</slot>
         </div>
 
-        <v-fade-transition mode="out-in">
-            <div id="overlay" v-if="busy">
-                <div id="overlayContent">
-                    <slot name="overlay">
-                        <v-progress-circular
-                            :size="spinnerSize"
-                            :width="spinnerWidth"
-                            :color="spinnerColor"
-                            :indeterminate="percent < 0"
-                            :model-value="roundedPercent"
-                            id="spinner"
-                        >
-                            <span v-if="roundedPercent >= 0" class="font-weight-bold">{{ roundedPercent }}%</span>
-                        </v-progress-circular>
-                        <p v-if="msg" class="mt-5" align="center">{{ localization.tt(msg) }}</p>
-                    </slot>
-                </div>
-            </div>
-        </v-fade-transition>
+        <Busy overlay v-bind="{ ...$props, ...$attrs }">
+            <template #spinner="{ message, percent }">
+                <slot name="overlay" :message="message" :percent="percent" />
+            </template>
+        </Busy>
     </div>
 </template>
 
@@ -36,15 +29,13 @@ A nice busy overlay that wraps the whole slot content and shades it a bit.
 import { ref, computed } from "vue";
 import { localization } from "jsl/Localization";
 
+import Busy from "./utils/Busy.vue";
+
 const props = defineProps({
     // If true, the form is assumed to be busy
     busy: { type: Boolean, required: false, default: false },
-    
-    // A message to display.
-    msg: { type: String, required: false, default: "" },
-    
-    // A percentage to display. If negative, the spinner is indeterminate
-    percent: { type: Number, required: false, default: -1 },
+
+    // Ensure these defaults. (might be different from the busy component)
 
     // If the default overlay is used, this defines the size of the v-progress-circular
     spinnerSize: { required: false, default: 70 },
@@ -53,23 +44,11 @@ const props = defineProps({
     // If the default overlay is used, this defines the color of the v-progress-circular
     spinnerColor: { type: String, required: false, default: "primary" },
 });
-
-const roundedPercent = computed(() => Math.round(props.percent));
 </script>
 
 <style scoped>
 #container {
     position: relative;
-}
-
-#overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
 }
 
 #content {
@@ -80,19 +59,5 @@ const roundedPercent = computed(() => Math.round(props.percent));
 .busyContent {
     opacity: 0.33;
     filter: blur(2px);
-}
-
-#spinner {
-    left: 50%;
-    transform: translateX(-50%);
-}
-
-#overlayContent {
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    position: absolute;
-    width: 100%;
-    padding: 1em;
 }
 </style>
